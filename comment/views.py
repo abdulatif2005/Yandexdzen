b1e8ee2c0c686b48cdbcf259e399e4e5f8bcd58f
Абдулатиф Имаров <abdulatifimarov@gmail.com>
2025-02-26 14:57:19 +0600

    добавлены api views для Comment

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import CommentListCreateSerializer, Comment
+from .permissions import IsStaffOrReadOnly
 
-# Create your views here.
+
+class CommentListAPIView(generics.ListAPIView):
+    serializer_class = CommentListCreateSerializer
+    lookup_url_kwarg = "post_id"
+
+    def get_queryset(self):
+        post_id = self.kwargs[self.lookup_url_kwarg]
+        quryset = Comment.objects.filter(post_id=post_id)
+        return quryset
+
+
+class CommentCreateAPIView(generics.CreateAPIView):
+    serializer_class = CommentListCreateSerializer
+    lookup_url_kwarg = "post_id"
+
+    def perform_create(self, serializer):
+        serializer.save(post_id=self.kwargs[self.lookup_url_kwarg])
+
+
+class CommentUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentListCreateSerializer
+    lookup_url_kwarg = "comment_id"
+    permission_classes = [IsStaffOrReadOnly]
