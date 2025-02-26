6d2890e35891d1592c8de928d990f7e17950600a
Абдулатиф Имаров <abdulatifimarov@gmail.com>
2025-02-26 14:57:40 +0600

    добавлены api views для User

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import User, UserListSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
+    permission_classes = [IsAuthenticated]
